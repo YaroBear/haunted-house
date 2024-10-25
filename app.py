8f267793c89e63d8a338eab84a4940b1454a6fcb
Yaroslav Berejnoi <berejnoi.yaroslav@gmail.com>
2024-10-25 08:54:14 -0600

    inject local ip into index.html; host on 5001 to not interfere with airplay on mac hosts

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import serial, time, threading, serial.tools.list_ports
-from flask import Flask, send_from_directory, jsonify
+import serial, time, threading, serial.tools.list_ports, socket
+from flask import Flask, render_template_string, jsonify
 from pathlib import Path
 from vlc import MediaPlayer
 
@@ -40,13 +40,26 @@ def play_bach(seconds=16):
     process.play()
     threading.Timer(seconds, process.stop).start()
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 SERIAL_PORT = get_serial_port()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def index():
-    return send_from_directory('static', 'index.html')
+    local_ip = get_local_ip()
+    return render_template_string(open('static/index.html').read(), local_ip=local_ip)
 
 @app.route("/play")
 def haunted_house():
@@ -55,7 +68,9 @@ def haunted_house():
     play_thunder()
     play_bach()
 
-    # Start a thread to send serial commands
     threading.Thread(target=queue_lightning_serial_commands).start()
 
     return jsonify({"status": "done"})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
